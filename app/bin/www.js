b3142ab089fd8f0072628fb63e6386ec5dd86f52
김건우 <kgw010119@gmail.com>
2021-09-12 01:07:44 +0000

    로그 관리 모듈 winston 등록 및 설정

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,8 +1,10 @@
 "use strict";
 
 const app = require("../app");
+const logger = require("../src/config/logger");
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-	console.log("Engine On 우주로 ~");
+	logger.info(`${PORT} Engine On 우주로 ~`);
 });
